a256f38a457b4869c6972ee3e8620fbca480e0ff
JSPR Design <jspr.dsgn@gmail.com>
2018-08-15 15:53:34 +0800

    Sync

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,35 @@
+$('[data-toggle="popover"]').popover()
+
+
+
+
+
+
+
+$("#view-appointment").popover({
+  html : true,
+  placement: "right",
+  content: function() {
+    return $("#view-appointment-popover").html();
+  },
+  trigger: "click"
+});
+
+
+$(document).on('click', function (e) {
+  $('[data-toggle="popover"],[data-original-title]').each(function () {
+      //the 'is' for buttons that trigger popups
+      //the 'has' for icons within a button that triggers a popup
+      if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {                
+          (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  // fix for BS 3.3.6
+      }
+
+  });
+});
+
+
+
+
 var tabs = $('.tabs');
 var items = $('.tabs').find('li').length;
 var selector = $(".tabs").find(".selector");
