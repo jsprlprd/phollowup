3d048af6393a9b8d43e4f8fe7e73f0588c8f395b
Jasper Lepardo <jsprlprd@macbooks-MacBook-Pro.local>
2018-12-16 20:44:26 +0800

    sync

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -107,3 +107,94 @@ $(document).ready(function() {
 
 
 // $('#details-tab').tab('show')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SLIDER TOGGLE FUNCTION
+
+$(window).load(function() {
+  // var heading_height = $(".heading").height(); //get width automaticly
+  // $("#global-slider").animate({ "left": "-" + slider_width });
+});
+
+$(document).ready(function() {
+
+  var heading_height = $(".heading.fixed-top").innerHeight(); //get width automaticly
+
+  $('#main-panel').css('padding-top', heading_height);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('.heading').innerHeight();
+
+  $(window).scroll(function(event){
+      didScroll = true;
+  });
+
+  setInterval(function() {
+      if (didScroll) {
+          hasScrolled();
+          didScroll = false;
+      }
+  }, 250);
+
+  function hasScrolled() {
+      var st = $(this).scrollTop();
+
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight){
+          // Scroll Down
+          $('.heading').removeClass('nav-down').addClass('nav-up');
+      } else {
+          // Scroll Up
+          if(st + $(window).height() < $(document).height()) {
+              $('.heading').removeClass('nav-up').addClass('nav-down');
+          }
+      }
+
+      lastScrollTop = st;
+  }
+});
