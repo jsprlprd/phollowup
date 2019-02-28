62e2d4b855be1b370d8ca7c3dd9945f8c6dd5bc6
jsprlprd <jsprlprd@gmail.com>
2019-02-28 15:16:05 +0800

    sync

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -226,3 +226,41 @@ function increment(incrementor, target){
   value+=incrementor;
   document.getElementById(target).value = value;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+	// edit button
+	$(".btn-edit").click(function() {
+		$(this).hide();
+		var secID = $(this).closest(".list-group-item-action").attr("id");
+		$("#" + secID + " .btn-close").show();
+		$("#" + secID + " .btn-check").show();
+		$("#" + secID + " .list-group-item-view-mode").hide();
+		$("#" + secID + " .list-group-item-edit-mode").show();
+ 	});
+
+	// cancel button
+	$(".btn-close, .btn-check").click(function() {
+		var secID = $(this).closest(".list-group-item-action").attr("id");
+		$("#" + secID + " .btn-edit").show();
+		$("#" + secID + " .btn-close").hide();
+		$("#" + secID + " .btn-check").hide();
+		$("#" + secID + " .list-group-item-view-mode").show();
+		$("#" + secID + " .list-group-item-edit-mode").hide();
+	});
+
+});
